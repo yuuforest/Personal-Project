1cadb57ceead8dc77574f6dfd80c872c09aef4fd
yurrim <yurimkang20@gmail.com>
2023-07-04 16:31:38 +0900

    Upload. 체육복

--- python/탐욕법(Greedy)/체육복.py
+++ python/탐욕법(Greedy)/체육복.py
@@ -19,6 +19,6 @@ def solution(n, lost, reserve):
         
     return n - answer
 
-print(solution(5, [2, 4], [1, 3, 5]))
-print(solution(5, [2, 4], [3]))
-print(solution(3, [3], [1]))
+print(solution(5, [2, 4], [1, 3, 5]))   # 5
+print(solution(5, [2, 4], [3]))         # 4
+print(solution(3, [3], [1]))            # 2
