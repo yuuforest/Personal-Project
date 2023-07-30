795e209072fd5b86e9537d44ae5d3cbec1f27165
yurrim <yurimkang20@gmail.com>
2023-07-31 03:30:47 +0900

    [PCCP 모의고사] 체육대회

--- PCCP 모의고사/체육대회.py
+++ PCCP 모의고사/체육대회.py
@@ -1,22 +1,21 @@
 
 """ [PCCP 모의고사 #1] 체육대회 """
 
+import itertools
+
 def solution(ability):
     answer = 0
-
     game = len(ability[0])
 
-    ab = [[] for _ in range(game)]      # 게임별 학생의 능력치
+    npr = list(itertools.permutations(ability, game))   # 각 게임에 사람을 뽑는 경우의 수 
 
-    for i in range(len(ability)):
-        for j in range(game):
-            ab[j].append((i, ability[i][j]))
+    for g in npr:
+        count = 0
+        for i in range(game):
+            count += g[i][i]
+        answer = max(answer, count)
 
-    for i in range(game):
-        ab[i].sort(key=lambda x: x[1], reverse=True)
-    
-    print(ab)
     return answer
 
 print(solution([[40, 10, 10], [20, 5, 0], [30, 30, 30], [70, 0, 70], [100, 100, 100]]))     # 210
-# print(solution([[20, 30], [30, 20], [20, 30]]))                                             # 60
+print(solution([[20, 30], [30, 20], [20, 30]]))                                             # 60
