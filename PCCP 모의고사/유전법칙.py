d8b77e11f6d06e134396d2b7b8d22566777671c0
yurrim <yurimkang20@gmail.com>
2023-08-01 16:15:03 +0900

    [PCCP 모의고사 #1] 유전법칙

--- PCCP 모의고사/유전법칙.py
+++ PCCP 모의고사/유전법칙.py
@@ -1,22 +1,36 @@
 
 """ [PCCP 모의고사 #1] 유전법칙 """
-
-
-# 1. 각 완두콩은 같은 유전자끼리 교배하여 정확히 4개의 후손을 남긴다
-# 2. 잡종 완두콩 Rr은 RR Rr Rr rr 형질의 후손을 남긴다
-# 3. 순종 완두콩은 자신과 같은 형질의 후손을 4개 남긴다
-
-# 완두콩의 세대, 세대 내에서 몇번째 개체인지
     
+def check(stack):
+    while stack:
+        number = stack.pop()
+        if number == 0:
+            return "RR"
+        if number == 3:
+            return "rr"
+    return "Rr"
+
 
 def solution(queries):
     answer = []
-    
+    stack = []
+
+    for generation, order in queries:    # 완두콩의 세대, 세대 내에서 개체 순서
+        
+        stack.clear()
+
+        order -= 1
+        while generation > 1:
+            generation -= 1
+            stack.append(order % 4)
+            order //= 4
+
+        answer.append(check(stack))
 
     return answer
 
 
-print(solution([[3, 5]]))                       # ["RR"]
-print(solution([[3, 8], [2, 2]]))               # ["rr", "Rr"]
-print(solution([[3, 1], [2, 3], [3, 9]]))       # ["RR", "Rr", "RR"]
-print(solution([[4, 26]]))                      # ["Rr"]
+print(solution([[3, 5]]))                           # ["RR"]
+print(solution([[3, 8], [2, 2]]))                   # ["rr", "Rr"]
+print(solution([[3, 1], [2, 3], [3, 9]]))         # ["RR", "Rr", "RR"]
+print(solution([[4, 26]]))                        # ["Rr"]
