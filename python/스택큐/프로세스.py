a1cc7ecb0135724fdedf724a9c7bb45d2aa5f6ad
yurrim <yurimkang20@gmail.com>
2023-08-11 00:34:16 +0900

    Upload. 프로세스

--- python/스택큐/프로세스.py
+++ python/스택큐/프로세스.py
@@ -24,7 +24,7 @@ def solution(priorities, location):
 
     return answer
 
-# print(solution([2, 1, 3, 2], 2))            # 1
-# print(solution([1, 1, 9, 1, 1, 1], 0))      # 5
+print(solution([2, 1, 3, 2], 2))            # 1
+print(solution([1, 1, 9, 1, 1, 1], 0))      # 5
 
 print(solution([1, 1, 1, 2], 2))            # 4
