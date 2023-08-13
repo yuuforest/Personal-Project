e72a82fa02c75cab46a1dfc72c4aaaff29a4b285
yurrim <yurimkang20@gmail.com>
2023-08-13 23:51:38 +0900

    Upload. 베스트앨범

--- python/해시/베스트앨범.py
+++ python/해시/베스트앨범.py
@@ -2,7 +2,27 @@
 """ Level 3. 베스트앨범 """
 
 def solution(genres, plays):
+
     answer = []
+
+    Music = {}
+
+    for idx in range(len(genres)):
+
+        genre = genres[idx]
+        play = plays[idx]
+
+        Music.setdefault(genre, [0])
+        Music[genre][0] += play
+        Music[genre].append((play, idx))
+
+    Music = sorted(Music.items(), key=lambda x: x[1][0], reverse=True)
+
+    for music in Music:
+        
+        
+        print(music[1])
+
     return answer
 
 print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]))        # [4, 1, 3, 0]
