972aba0e932adf586b742ad7983ba1b465d336aa
yurrim <yurimkang20@gmail.com>
2023-08-18 02:41:12 +0900

    Upload. 같은 숫자는 싫어

--- python/스택큐/같은숫자는싫어.py
+++ python/스택큐/같은숫자는싫어.py
@@ -5,13 +5,13 @@ def solution(arr):
     answer = []
 
     answer.append(arr[0])
+    previous = arr[0]
 
     for num in arr[1:]:
 
-        if answer[-1] == num:
-            continue
-
-        answer.append(num)
+        if previous != num:
+            answer.append(num)
+            previous = num
         
     return answer
 
