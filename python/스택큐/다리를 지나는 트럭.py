955d96416363eee7106a964d0f8c974aad82e766
yurrim <yurimkang20@gmail.com>
2023-08-19 19:41:56 +0900

    Upload. 다리를 지나는 트럭

---
+++ python/스택큐/다리를 지나는 트럭.py
@@ -0,0 +1,11 @@
+
+""" Level 2. 다리를 지나는 트럭 """
+
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    return answer
+
+
+print(solution(2, 10, [7,4,5,6]))                               # 8
+print(solution(100, 100, [10]))                                 # 101
+print(solution(100, 100, [10,10,10,10,10,10,10,10,10,10]))      # 110
