23e6e4041869b1adba2ee3f5438c74d1edb80186
yurrim <yurimkang20@gmail.com>
2023-08-20 22:31:10 +0900

    Upload. 주식가격

--- python/스택큐/주식가격.py
+++ python/스택큐/주식가격.py
@@ -2,7 +2,19 @@
 """ Level 2. 주식가격 """
 
 def solution(prices):
-    answer = []
+
+    length = len(prices)
+    answer = [0 for _ in range(length)]
+
+    for idx, price in enumerate(prices[:length-1]):
+        
+        i = idx+1
+
+        while i < (length - 1) and price <= prices[i]:
+            i += 1
+
+        answer[idx] = i - idx
+
     return answer
 
 
