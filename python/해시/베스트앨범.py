aba44c1ce360c36afcec1af5e9f2943ee8c591fa
yurrim <yurimkang20@gmail.com>
2023-08-23 22:20:56 +0900

    Upload. 베스트앨범

--- python/해시/베스트앨범.py
+++ python/해시/베스트앨범.py
@@ -28,5 +28,5 @@ def solution(genres, plays):
 
 print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]))    # [4, 1, 3, 0]
 
-# print(solution(["classic", "pop", "classic", "classic"], [500, 600, 150, 800]))                 # [3, 0, 1]
-# print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 500, 150, 2500]))    # [4, 1, 0, 2]
+print(solution(["classic", "pop", "classic", "classic"], [500, 600, 150, 800]))                 # [3, 0, 1]
+print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 500, 150, 2500]))    # [4, 1, 0, 2]
