3b412ad2947689dc545b8b008a105f26767ebf64
yurrim <yurimkang20@gmail.com>
2023-08-23 22:18:03 +0900

    Upload. 베스트앨범

--- python/해시/베스트앨범.py
+++ python/해시/베스트앨범.py
@@ -7,26 +7,20 @@ def solution(genres, plays):
 
     answer = []
 
-    Music = defaultdict(lambda: [0])
+    count_genre = defaultdict(list)
+    count_play = defaultdict(int)
 
     for idx, (genre, play) in enumerate(zip(genres, plays)):
+        count_play[genre] += play
+        count_genre[genre].append((idx, play))
 
-        Music[genre][0] += play
-        Music[genre].append((play, idx))
-
-    Music = sorted(Music.items(), key=lambda x: x[1][0], reverse=True)
-
-    for music in Music:
-        
-        temp = sorted(music[1][1:], key=lambda x:(-x[0], x[1]))
-
-        answer.append(temp[0][1])
-        if len(temp) > 1: 
-            answer.append(temp[1][1])
+    for (g, _) in sorted(count_play.items(), key=lambda x:-x[1]):
+        for (p, _) in sorted(count_genre[g], key=lambda x:(-x[1], x[0])) [:2]:
+            answer.append(p)
 
     return answer
 
 print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]))    # [4, 1, 3, 0]
 
-print(solution(["classic", "pop", "classic", "classic"], [500, 600, 150, 800]))                 # [3, 0, 1]
-print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 500, 150, 2500]))    # [4, 1, 0, 2]
+# print(solution(["classic", "pop", "classic", "classic"], [500, 600, 150, 800]))                 # [3, 0, 1]
+# print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 500, 150, 2500]))    # [4, 1, 0, 2]
