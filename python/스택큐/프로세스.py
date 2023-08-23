a81ff3195a400d172067c8e07aaa51ae01853991
yurrim <yurimkang20@gmail.com>
2023-08-23 22:17:53 +0900

    Upload. 프로세스

--- python/스택큐/프로세스.py
+++ python/스택큐/프로세스.py
@@ -12,7 +12,7 @@ def solution(priorities, location):
         
         temp = P.pop(0)
 
-        if temp[0] < max(P)[0]:
+        if any(temp[0] < p[0] for p in P):
             P.append(temp)
             continue
         
