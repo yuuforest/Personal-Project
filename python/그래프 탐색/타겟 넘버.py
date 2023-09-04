d632498c8c10655434226dae4367b46746d44028
yurrim <yurimkang20@gmail.com>
2023-09-04 21:45:37 +0900

    Upload. 타겟 넘버

--- python/그래프 탐색/타겟 넘버.py
+++ python/그래프 탐색/타겟 넘버.py
@@ -1,25 +1,30 @@
 
 """ 레벨 2. 타겟 넘버 """
 
-count = 0
+from collections import deque
 
 def solution(numbers, target):
-    dfs(numbers, target, 0, 0)
-    return count
+    queue = deque()
+
+    queue.append(numbers[0])
+    queue.append(-numbers[0])
+
+    idx = 1
+
+    while idx < len(numbers):
+
+        for _ in range(len(queue)):
+
+            num = queue.popleft()
+
+            queue.append(num + numbers[idx])
+            queue.append(num - numbers[idx])
+
+        idx += 1
+
+    return queue.count(target)
 
 
-def dfs(numbers, target, idx, sum):
-
-    global count
-    if idx == len(numbers):
-        if sum == target:
-            count += 1
-        return
-    
-    dfs(numbers, target, idx+1, sum-numbers[idx])
-    dfs(numbers, target, idx+1, sum+numbers[idx])
-
-
-### 실행
+## 실행
 print(solution([1, 1, 1, 1, 1], 3))     # 5
 print(solution([4, 1, 2, 1], 4))        # 2
