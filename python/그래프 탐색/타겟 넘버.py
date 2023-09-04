cc4f99228a322af32ad3541878a2884c75e0ddcf
yurrim <yurimkang20@gmail.com>
2023-09-04 20:35:54 +0900

    Upload. 타겟 넘버

---
+++ python/그래프 탐색/타겟 넘버.py
@@ -0,0 +1,11 @@
+
+""" 레벨 2. 타겟 넘버 """
+
+def solution(numbers, target):
+    answer = 0
+    return answer
+
+
+### 실행
+print(solution([1, 1, 1, 1, 1], 3))     # 5
+print(solution([4, 1, 2, 1], 4))        # 2
