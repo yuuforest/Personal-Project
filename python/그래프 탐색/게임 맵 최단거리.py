2933b02fd5538ba8272d1cb11a64042c2226aac7
yurrim <yurimkang20@gmail.com>
2023-09-07 13:24:41 +0900

    Upload. 게임 맵 최단거리

--- python/그래프 탐색/게임 맵 최단거리.py
+++ python/그래프 탐색/게임 맵 최단거리.py
@@ -1,9 +1,40 @@
 
 """ 레벨 2. 게임 맵 최단거리 """
 
+from collections import deque
+
 def solution(maps):
-    answer = 0
-    return answer
+    row = len(maps)
+    col = len(maps[0])
+
+    dr = [0, 0, 1, -1]
+    dc = [-1, 1, 0, 0]
+
+    visited = [[False] * col for _ in range(row)]
+
+    queue = deque()
+
+    queue.append((0, 0, 1))
+    visited[0][0] = True
+
+    while queue:
+
+        r, c, count = queue.popleft()
+
+        if r == row-1 and c == col-1:
+            return count
+
+        for d in range(4):
+            nr = r + dr[d]
+            nc = c + dc[d]
+
+            if nr < 0 or nc < 0 or nr >= row or nc >= col or visited[nr][nc] or not maps[nr][nc]:
+                continue
+            
+            queue.append((nr, nc, count+1))
+            visited[nr][nc] = True
+
+    return -1
 
 
 print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))      # 11
