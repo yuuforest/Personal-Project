5b2c43fb3b5425b637ec4762e68ebaf33e3839bc
yurrim <yurimkang20@gmail.com>
2023-09-11 14:33:45 +0900

    Upload. 네트워크

--- python/그래프 탐색/네트워크.py
+++ python/그래프 탐색/네트워크.py
@@ -3,7 +3,23 @@
 
 def solution(n, computers):
     answer = 0
+    visited = [False] * n
+
+    for idx in range(n):
+        if not visited[idx]:
+            answer += 1
+            dfs(n, computers, idx, visited)
+
     return answer
+
+
+def dfs(n, computers, node, visited):
+
+    visited[node] = True
+
+    for com in range(n):
+        if not visited[com] and computers[node][com]:
+            dfs(n, computers, com, visited)
 
 
 print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))       # 2
