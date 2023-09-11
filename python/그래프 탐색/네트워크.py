b79ceeb4c9128f5aedc8a581d2ad58a141ba7232
yurrim <yurimkang20@gmail.com>
2023-09-11 11:53:15 +0900

    Upload. 네트워크

---
+++ python/그래프 탐색/네트워크.py
@@ -0,0 +1,10 @@
+
+""" 레벨 3. 네트워크 """
+
+def solution(n, computers):
+    answer = 0
+    return answer
+
+
+print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))       # 2
+print(solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]]))       # 1
