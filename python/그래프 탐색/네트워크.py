539378c0800672c336d46db51baeb69937827f15
yurrim <yurimkang20@gmail.com>
2023-09-11 14:45:32 +0900

    Upload. 네트워크

--- python/그래프 탐색/네트워크.py
+++ python/그래프 탐색/네트워크.py
@@ -1,6 +1,8 @@
 
 """ 레벨 3. 네트워크 """
 
+from collections import deque
+
 def solution(n, computers):
     answer = 0
     visited = [False] * n
@@ -8,18 +10,28 @@ def solution(n, computers):
     for idx in range(n):
         if not visited[idx]:
             answer += 1
-            dfs(n, computers, idx, visited)
+            visited = bfs(n, computers, idx, visited)
 
     return answer
 
 
-def dfs(n, computers, node, visited):
+def bfs(n, computers, node, visited):
 
+    queue = deque()
+    
+    queue.append(node)
     visited[node] = True
 
-    for com in range(n):
-        if not visited[com] and computers[node][com]:
-            dfs(n, computers, com, visited)
+    while queue:
+
+        now = queue.popleft()
+
+        for idx in range(n):
+            if not visited[idx] and computers[now][idx]:
+                queue.append(idx)
+                visited[idx] = True
+
+    return visited
 
 
 print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))       # 2
