a507837591a4dc91ed17c4549190f6a201ea05f8
yurrim <yurimkang20@gmail.com>
2023-09-20 23:05:37 +0900

    Upload. 아이템 줍기

---
+++ python/그래프 탐색/아이템 줍기.py
@@ -0,0 +1,13 @@
+
+""" 레벨 3. 아이템 줍기 """
+
+def solution(rectangle, characterX, characterY, itemX, itemY):
+    answer = 0
+    return answer
+
+
+print(solution([[1,1,7,4],[3,2,5,5],[4,3,6,9],[2,6,8,8]], 1, 3, 7, 8))      # 17
+print(solution([[1,1,8,4],[2,2,4,9],[3,6,9,8],[6,3,7,7]], 9, 7, 6, 1))      # 11
+print(solution([[1,1,5,7]], 1, 1, 4, 7))                                    # 9
+print(solution([[2,1,7,5],[6,4,10,10]], 3, 1, 7, 10))                       # 15
+print(solution([[2,2,5,5],[1,3,6,4],[3,1,4,6]], 1, 4, 6, 3))                # 10
