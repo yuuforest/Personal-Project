69b77fd90507757d208ae96454daa84bf57bcc27
yurrim <yurimkang20@gmail.com>
2023-09-21 15:15:21 +0900

    Upload. H-Index

--- python/정렬/H-Index.py
+++ python/정렬/H-Index.py
@@ -1,2 +1,28 @@
 
 """ 레벨 2. H-Index """
+
+def solution(citations):
+    answer = 0
+
+    start = 0
+    end = max(citations)
+
+    while start <= end:
+        
+        mid = (start + end) // 2
+
+        check = 0
+        for c in citations:
+            if c >= mid:
+                check += 1
+        
+        if check >= mid:
+            answer = mid
+            start += 1
+        else:
+            end -= 1
+
+    return answer
+
+
+print(solution([3, 0, 6, 1, 5]))        # 3
