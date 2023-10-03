b7886a5f991ebab0a0e25306e29d8284fc4465e5
yurrim <yurimkang20@gmail.com>
2023-10-03 17:05:32 +0900

    [PCCP 모의고사 #2] 신입사원 교육

--- PCCP 모의고사/신입사원 교육.py
+++ PCCP 모의고사/신입사원 교육.py
@@ -1,10 +1,24 @@
 
 """ [PCCP 모의고사 #2] 신입사원 교육 """
 
-def solution(ability, number):
-    answer = 0
-    return answer
+import heapq
 
+def solution(ability, number):
+
+    q = []
+    for a in ability:
+        heapq.heappush(q, a)
+
+    for _ in range(number):
+
+        total = 0
+        for _ in range(2):
+            total += heapq.heappop(q)
+        
+        heapq.heappush(q, total)
+        heapq.heappush(q, total)
+
+    return sum(q)
 
 print(solution([10, 3, 7, 2], 2))       # 37
 print(solution([1, 2, 3, 4], 3))        # 26
