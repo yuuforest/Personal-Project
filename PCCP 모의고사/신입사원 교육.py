9f4f5c88009565cddccdd0e424858f508c66f8e7
yurrim <yurimkang20@gmail.com>
2023-10-03 16:36:49 +0900

    [PCCP 모의고사 #2] 신입사원 교육

---
+++ PCCP 모의고사/신입사원 교육.py
@@ -0,0 +1,10 @@
+
+""" [PCCP 모의고사 #2] 신입사원 교육 """
+
+def solution(ability, number):
+    answer = 0
+    return answer
+
+
+print(solution([10, 3, 7, 2], 2))       # 37
+print(solution([1, 2, 3, 4], 3))        # 26
