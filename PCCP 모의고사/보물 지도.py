d1a543344fe22aaac3782a18f881bf6b18e14a88
yurrim <yurimkang20@gmail.com>
2023-10-03 17:08:15 +0900

    [PCCP 모의고사 #2] 보물 지도

---
+++ PCCP 모의고사/보물 지도.py
@@ -0,0 +1,10 @@
+
+""" [PCCP 모의고사 #2] 보물 지도 """
+
+def solution(n, m, hole):
+    answer = 0
+    return answer
+
+
+print(solution(4, 4, [[2, 3], [3, 3]]))                                                                 # 5
+print(solution(5, 4, [[1, 4], [2, 1], [2, 2], [2, 3], [2, 4], [3, 3], [4, 1], [4, 3], [5, 3]]))         # -1
