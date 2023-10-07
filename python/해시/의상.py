a7442ca2be9a99bf7bc63c098ae3278e96d797d9
yurrim <yurimkang20@gmail.com>
2023-10-07 16:59:44 +0900

    Upload. 의상

---
+++ python/해시/의상.py
@@ -0,0 +1,9 @@
+
+""" 레벨 2. 의상 """
+
+def solution(clothes):
+    answer = 0
+    return answer
+
+print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]))     # 5
+print(solution([["crow_mask", "face"], ["blue_sunglasses", "face"], ["smoky_makeup", "face"]]))                 # 3
