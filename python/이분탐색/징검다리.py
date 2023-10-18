29ebdd38c409a22fc1e8c3b93f249c342c74450b
yurrim <yurimkang20@gmail.com>
2023-10-18 23:09:31 +0900

    Upload. 징검다리

--- python/이분탐색/징검다리.py
+++ python/이분탐색/징검다리.py
@@ -2,9 +2,10 @@
 """ 레벨 4. 징검다리 """
 
 def solution(distance, rocks, n):
-    answer = 0
 
-    rocks = [0] + sorted(rocks) + [distance]
+    rocks.sort()
+
+    rocks = sorted(rocks) + [distance]
 
     answer, start, end = 0, 0, distance
 
@@ -12,22 +13,23 @@ def solution(distance, rocks, n):
         mid = (start + end) // 2
 
         count, i = 0, 0
-        for idx in range(1, len(rocks)):
-            if rocks[idx] - rocks[i] < mid:
+        for rock in rocks:
+            if rock - i < mid:
                 count += 1
             else:
-                i = idx
+                i = rock
 
-        if count == n:
-            answer = max(mid, answer)
-            break
-        elif count < n:
-            start = mid + 1
-        else:
+        if count > n:
             end = mid - 1
+        else:
+            answer = mid
+            start = mid + 1
 
     return answer
 
 print(solution(25, [2, 14, 11, 21, 17], 2))     # 4
 
 print(solution(16, [4, 8, 11], 2))              # 8
+print(solution(1, [1], 1))                      # 1
+print(solution(23, [3, 6, 9, 10, 14, 17], 2))   # 3
+print(solution(5, [3], 1))                      # 5
