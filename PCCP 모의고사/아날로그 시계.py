81e720ce0149f91061c0deac7f01aaa45ff47991
yurrim <yurimkang20@gmail.com>
2024-03-13 14:55:54 +0900

    [PCCP 기출문제 #1] 아날로그 시계

---
+++ PCCP 모의고사/아날로그 시계.py
@@ -0,0 +1,18 @@
+
+""" [PCCP 기출문제 #3] 아날로그 시계 """
+
+def solution(h1, m1, s1, h2, m2, s2):
+
+    answer = -1
+    
+    
+
+    return answer
+
+print(solution(0, 5, 30, 0, 7, 0))          # 2
+print(solution(12, 0, 0, 12, 0, 30))        # 1
+print(solution(0, 6, 1, 0, 6, 6))           # 0
+print(solution(11, 59, 30, 12, 0, 0))       # 1
+print(solution(11, 58, 59, 11, 59, 0))      # 1
+print(solution(1, 5, 5, 1, 5, 6))           # 2
+print(solution(0, 0, 0, 23, 59, 59))        # 2852
