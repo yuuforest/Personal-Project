8013e8c64cc544d6d658ed4c26a9f38e133f9cf8
yurrim <yurimkang20@gmail.com>
2024-03-13 15:14:46 +0900

    Upload. 프로세스

---
+++ python/자료구조/프로세스.py
@@ -0,0 +1,32 @@
+
+""" Level 2. 프로세스 """
+
+def solution(priorities, location):
+    answer = 0
+
+    P = [(pr, idx) for idx, pr in enumerate(priorities)]  
+    N = len(priorities) - 1  
+
+    # while len(P) > 1:
+    while answer < N:
+        
+        temp = P.pop(0)
+
+        if temp[0] < max(P)[0]:
+            P.append(temp)
+            continue
+        
+        answer += 1
+
+        if temp[1] == location:
+            break
+    
+    else:
+        answer += 1
+
+    return answer
+
+print(solution([2, 1, 3, 2], 2))            # 1
+print(solution([1, 1, 9, 1, 1, 1], 0))      # 5
+
+print(solution([1, 1, 1, 2], 2))            # 4
