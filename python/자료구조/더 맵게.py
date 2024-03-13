68f72186c84d1614c8dd06896fb886ce65ed0f52
yurrim <yurimkang20@gmail.com>
2024-03-13 15:34:55 +0900

    Upload. 더 맵게

--- python/자료구조/더 맵게.py
+++ python/자료구조/더 맵게.py
@@ -1,8 +1,25 @@
 
 """ Level 2. 더 맵게 """
 
+from heapq import heappush, heappop, heapify
+
 def solution(scoville, K):
+    
     answer = 0
-    return answer
+    slen = len(scoville)
+
+    heapify(scoville)
+
+    while scoville[0] < K and slen > 1:
+
+        num1 = heappop(scoville)
+        num2 = heappop(scoville)
+        heappush(scoville, num1 + num2 * 2)
+
+        answer += 1
+        slen -= 1
+
+    return answer if scoville[0] >= K else -1
 
 print(solution([1, 2, 3, 9, 10, 12], 7))        # 2
+print(solution([1, 1, 1, 1, 1, 1], 1500))          # -1
