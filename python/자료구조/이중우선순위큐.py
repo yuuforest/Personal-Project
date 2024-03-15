5b5f4a1dfd167c25e0e67993c4f612b23266f97b
yurrim <yurimkang20@gmail.com>
2024-03-15 15:29:43 +0900

    Upload. 이중우선순위큐

---
+++ python/자료구조/이중우선순위큐.py
@@ -0,0 +1,9 @@
+
+""" Level 3. 이중우선순위큐 """
+
+def solution(operations):
+    answer = []
+    return answer
+
+print(solution(["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]))                         # [0,0]
+print(solution(["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"]))        # [333,-45]
