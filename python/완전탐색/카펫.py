5d2ee9f9012d81777b4d329b6fc27284d15edbd2
yurrim <yurimkang20@gmail.com>
2024-03-16 15:31:32 +0900

    Upload. 카펫

--- python/완전탐색/카펫.py
+++ python/완전탐색/카펫.py
@@ -1,9 +1,21 @@
 
 """ Level 2. 카펫 """
 
+import math
+
 def solution(brown, yellow):
-    answer = []
-    return answer
+
+    # 노란색의 개수 = (가로 - 2) * (세로 - 2)
+    # 갈색의 개수 = 가로 * 2 + (세로 - 2) * 2 = (가로 + 세로 - 2) * 2
+
+    total = (brown // 2) + 2
+
+    for width in range(math.ceil(total / 2), total):
+        
+        height = total - width
+
+        if yellow == (width - 2) * (height - 2):
+            return [width, height] 
 
 print(solution(10, 2))      # [4, 3]
 print(solution(8, 1))       # [3, 3]
