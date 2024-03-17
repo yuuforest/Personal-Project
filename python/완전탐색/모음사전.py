f4bb9d5b8a6abcbf90d17ba7e63a74ff8070fa0a
yurrim <yurimkang20@gmail.com>
2024-03-17 18:34:54 +0900

    Upload. 모음사전

---
+++ python/완전탐색/모음사전.py
@@ -0,0 +1,11 @@
+
+""" Level 2. 모음사전 """
+
+def solution(word):
+    answer = 0
+    return answer
+
+print(solution("AAAAE"))        # 6
+print(solution("AAAE"))         # 10
+print(solution("I"))            # 1563
+print(solution("EIO"))          # 1189
