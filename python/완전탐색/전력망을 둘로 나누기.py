7606b5bd879306c0f19210307c65d3c458f71328
yurrim <yurimkang20@gmail.com>
2024-03-17 18:35:12 +0900

    Upload. 전력망을 둘로 나누기

---
+++ python/완전탐색/전력망을 둘로 나누기.py
@@ -0,0 +1,10 @@
+
+""" Level 2. 전력망을 둘로 나누기 """
+
+def solution(n, wires):
+    answer = -1
+    return answer
+
+print(solution(9, [[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]]))       # 3     
+print(solution(4, [[1,2],[2,3],[3,4]]))                                     # 0
+print(solution(7, [[1,2],[2,7],[3,7],[3,4],[4,5],[6,7]]))                   # 1
