63b25f367f5c5806fdbb13f8cd9c0926b9510f52
yurrim <yurimkang20@gmail.com>
2024-03-19 19:20:08 +0900

    Upload. 구명보트

---
+++ python/탐욕법(그리디)/구명보트.py
@@ -0,0 +1,9 @@
+
+""" Level 2. 구명보트 """ 
+
+def solution(people, limit):
+    answer = 0
+    return answer
+
+print(solution([70, 50, 80, 50], 100))      # 3
+print(solution([70, 80, 50], 100))          # 3
