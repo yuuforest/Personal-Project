2915be852b8261dc950e45ab1c149e77580f8bef
yurrim <yurimkang20@gmail.com>
2024-03-20 12:29:01 +0900

    Upload. 모음사전

--- python/완전탐색/모음사전.py
+++ python/완전탐색/모음사전.py
@@ -1,9 +1,19 @@
 
 """ Level 2. 모음사전 """
 
+from itertools import product
+
 def solution(word):
-    answer = 0
-    return answer
+    answer = []
+
+    alphabet = ['A', 'E', 'I', 'O', 'U']
+
+    for idx in range(1, 6):
+        for temp in list(product(alphabet, repeat=idx)):
+            answer.append(''.join(temp))
+
+    # answer.sort()
+    return sorted(answer).index(word) + 1
 
 print(solution("AAAAE"))        # 6
 print(solution("AAAE"))         # 10
