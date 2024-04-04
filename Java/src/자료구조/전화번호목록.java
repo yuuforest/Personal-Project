2ad15b91c8086c1dcb30ca250a9a399a1d2f7354
yurrim <yurimkang20@gmail.com>
2024-04-04 14:05:36 +0900

    Upload. 전화번호목록

--- Java/src/자료구조/전화번호목록.java
+++ Java/src/자료구조/전화번호목록.java
@@ -1,5 +1,32 @@
 package 자료구조;
 
+import java.util.Arrays;
+
 public class 전화번호목록 {
+
+    public static void main(String[] args) {
+
+        String[] p1 = {"119", "97674223", "1195524421"};
+        System.out.println(solution(p1));
+
+        String[] p2 = {"123","456","789"};
+        System.out.println(solution(p2));
+
+        String[] p3 = {"12","123","1235","567","88"};
+        System.out.println(solution(p3));
+    }
     
+    public static boolean solution(String[] phone_book) {
+        
+        Arrays.sort(phone_book);
+
+        for (int i = 1; i < phone_book.length; i++) {
+            if (phone_book[i-1].length() <= phone_book[i].length()
+                && phone_book[i].substring(0, phone_book[i-1].length()).equals(phone_book[i-1])) {
+                    return false;
+            }
+        }
+        
+        return true;
+    }
 }
