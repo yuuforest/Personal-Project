2beecf96d76589283741c6d1bb78c8446074a80a
yurrim <yurimkang20@gmail.com>
2024-04-04 13:26:31 +0900

    Upload. 완주하지못한선수

---
+++ Java/src/자료구조/완주하지못한선수.java
@@ -0,0 +1,44 @@
+package 자료구조;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 완주하지못한선수 {
+
+    public static void main(String[] args) {
+
+        String[] p1 = {"leo", "kiki", "eden"};
+        String[] c1 = {"eden", "kiki"};
+        System.out.println(solution(p1, c1));
+
+        String[] p2 = {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] c2 = {"josipa", "filipa", "marina", "nikola"};
+        System.out.println(solution(p2, c2));
+
+        String[] p3 = {"mislav", "stanko", "mislav", "ana"};
+        String[] c3 = {"stanko", "ana", "mislav"};
+        System.out.println(solution(p3, c3));
+
+    }
+
+    public static String solution(String[] participant, String[] completion) {
+
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+
+        for (String c : completion) {
+            map.replace(c, map.get(c) - 1);
+        }
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 0) {
+                return entry.getKey().toString();
+            }
+        }
+
+        return null;
+    }
+}
