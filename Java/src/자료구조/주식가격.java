2b30cf91327e74c129667e09ec44f666c8fa4edf
yurrim <yurimkang20@gmail.com>
2024-04-05 20:26:21 +0900

    Upload. 주식가격

---
+++ Java/src/자료구조/주식가격.java
@@ -0,0 +1,17 @@
+package 자료구조;
+
+import java.util.*;
+
+public class 주식가격 {
+
+    public static void main(String[] args) {
+
+        int[] p1 = {1, 2, 3, 2, 3};
+        System.out.println(Arrays.toString(solution(p1)));
+    }
+
+    public static int[] solution(int[] prices) {
+        int[] answer = {};
+        return answer;
+    }
+}
