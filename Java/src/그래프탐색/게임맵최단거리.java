b4039cfad0b4db027aeb0894eaf1fbbe0862f698
yurrim <yurimkang20@gmail.com>
2024-04-08 01:46:16 +0900

    Upload. 게임 맵 최단거리

--- Java/src/그래프탐색/게임맵최단거리.java
+++ Java/src/그래프탐색/게임맵최단거리.java
@@ -1,5 +1,7 @@
 package 그래프탐색;
 
+import java.util.*;
+
 public class 게임맵최단거리 {
 
     public static void main(String[] args) {
@@ -9,12 +11,40 @@ public class 게임맵최단거리 {
 
         int[][] m2 = {{1,0,1,1,1}, {1,0,1,0,1}, {1,0,1,1,1}, {1,1,1,0,0}, {0,0,0,0,1}};
         System.out.println(solution(m2));
-
     }
 
     public static int solution(int[][] maps) {
-        int answer = 0;
-        return answer;
+
+        int R = maps.length, C = maps[0].length;
+        
+        int[] dr = {0, 0, -1, 1};
+        int[] dc = {-1, 1, 0, 0};
+
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] selected = new boolean[R][C];
+
+        q.add(new int[] {0, 0, 1});
+        selected[0][0] = true;
+
+        while (!q.isEmpty()) {
+
+            int[] now = q.poll();
+
+            if (now[0] == (R-1) && now[1] == (C-1)) return now[2];
+
+            for (int d = 0; d < 4; d++) {
+
+                int nr = now[0] + dr[d];
+                int nc = now[1] + dc[d];
+
+                if (nr < 0 || nr >= R || nc < 0 || nc >= C) continue;
+                if (selected[nr][nc] || maps[nr][nc] == 0) continue;
+
+                q.add(new int[] {nr, nc, now[2] + 1});
+                selected[nr][nc] = true;
+            }
+        }
+
+        return -1;
     }
-    
 }
