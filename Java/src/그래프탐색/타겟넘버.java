d5ee83e56bd0a8d85d2d621a40053b8cb6b77bd2
yurrim <yurimkang20@gmail.com>
2024-04-08 01:12:56 +0900

    Upload. 타겟 넘버

---
+++ Java/src/그래프탐색/타겟넘버.java
@@ -0,0 +1,21 @@
+package 그래프탐색;
+
+public class 타겟넘버 {
+    
+    public static void main(String[] args) {
+
+        int[] n1 = {1, 1, 1, 1, 1};
+        int t1 = 3;
+        System.out.println(solution(n1, t1));
+
+        int[] n2 = {4, 1, 2, 1};
+        int t2 = 4;
+        System.out.println(solution(n2, t2));
+
+    }
+
+    public static int solution(int[] numbers, int target) {
+        int answer = 0;
+        return answer;
+    }
+}
