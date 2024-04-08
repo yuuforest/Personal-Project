efbfc770eb82de3c44b171a9620f507b19e6fc76
yurrim <yurimkang20@gmail.com>
2024-04-09 00:24:48 +0900

    Upload. 네트워크

---
+++ Java/src/그래프탐색/네트워크.java
@@ -0,0 +1,24 @@
+package 그래프탐색;
+
+public class 네트워크 {
+    
+    public static void main(String[] args) {
+
+        int n1 = 3;
+        int[][] c1 = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+        System.out.println(solution(n1, c1));
+
+        int n2 = 3;
+        int[][] c2 = {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
+        System.out.println(solution(n2, c2));
+    }
+
+    public static int solution(int n, int[][] computers) {
+
+        int answer = 0;
+        
+        
+
+        return answer;
+    }
+}
