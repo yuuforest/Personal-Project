9246ff7d73b65207c092bb59fb6e804ee450b6b0
yurrim <yurimkang20@gmail.com>
2024-04-08 23:18:10 +0900

    Upload. 모의고사

---
+++ Java/src/완전탐색/모의고사.java
@@ -0,0 +1,18 @@
+package 완전탐색;
+
+public class 모의고사 {
+    
+    public static void main(String[] args) {
+
+        int[] a1 = {1,2,3,4,5};
+        System.out.println(solution(a1));
+
+        int[] a2 = {1,3,2,4,2};
+        System.out.println(solution(a2));
+    }
+
+    public static int[] solution(int[] answers) {
+        int[] answer = {};
+        return answer;
+    }
+}
