1949face3e36574b5c7df4356265b0c3cfd0bdf5
yurrim <yurimkang20@gmail.com>
2024-04-09 15:09:44 +0900

    Upload. 큰 수 만들기

---
+++ Java/src/그리디/큰수만들기.java
@@ -0,0 +1,24 @@
+package 그리디;
+
+public class 큰수만들기 {
+    
+    public static void main(String[] args) {
+
+        String n1 = "1924";
+        int k1 = 2;
+        System.out.println(solution(n1, k1));
+
+        String n2 = "1231234";
+        int k2 = 3;
+        System.out.println(solution(n2, k2));
+
+        String n3 = "4177252841";
+        int k3 = 4;
+        System.out.println(solution(n3, k3));
+    }
+
+    public static String solution(String number, int k) {
+        String answer = "";
+        return answer;
+    }
+}
