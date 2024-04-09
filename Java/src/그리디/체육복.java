2b7a75f00c87239735bafdf85f89b05ea4bdf888
yurrim <yurimkang20@gmail.com>
2024-04-09 15:09:31 +0900

    Upload. 체육복

---
+++ Java/src/그리디/체육복.java
@@ -0,0 +1,27 @@
+package 그리디;
+
+public class 체육복 {
+
+    public static void main(String[] args) {
+
+        int n1 = 5;
+        int[] l1 = {2, 4};
+        int[] r1 = {1, 3, 5};
+        System.out.println(solution(n1, l1, r1));
+
+        int n2 = 5;
+        int[] l2 = {2, 4};
+        int[] r2 = {3};
+        System.out.println(solution(n2, l2, r2));
+
+        int n3 = 3;
+        int[] l3 = {3};
+        int[] r3 = {1};
+        System.out.println(solution(n3, l3, r3));
+    }
+
+    public static int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        return answer;
+    }
+}
