ac73380384a09cbf403571420b87bf9f1c767b3d
yurrim <yurimkang20@gmail.com>
2024-04-14 00:36:56 +0900

    Upload. 구명보트

--- Java/src/그리디/구명보트.java
+++ Java/src/그리디/구명보트.java
@@ -13,14 +13,35 @@ public class 구명보트 {
         int[] p2 = {70, 80, 50};
         int l2 = 100;
         System.out.println(solution(p2, l2));
+
+        int[] p3 = {50};
+        int l3 = 100;
+        System.out.println(solution(p3, l3));
     }
 
     public static int solution(int[] people, int limit) {
         
+        Arrays.sort(people);
+        
+        int start = 0;
+        int end = people.length - 1;
+        
         int answer = 0;
-        
-        
-        
+        while (start <= end) {
+            
+            while (start <= end && people[start] + people[end] > limit) {
+                answer++;
+                end--;
+            }
+            
+            if (start <= end) {
+                answer++;
+            }
+
+            start++;
+            end--;
+        }
+
         return answer;
     }
 }
