dea42949a65cf55c3e67f906d458039af7b0c773
yurrim <yurimkang20@gmail.com>
2024-04-14 00:14:16 +0900

    Upload. 구명보트

---
+++ Java/src/그리디/구명보트.java
@@ -0,0 +1,26 @@
+package 그리디;
+
+import java.util.*;
+
+public class 구명보트 {
+
+    public static void main(String[] args) {
+
+        int[] p1 = {70, 50, 80, 50};
+        int l1 = 100;
+        System.out.println(solution(p1, l1));
+
+        int[] p2 = {70, 80, 50};
+        int l2 = 100;
+        System.out.println(solution(p2, l2));
+    }
+
+    public static int solution(int[] people, int limit) {
+        
+        int answer = 0;
+        
+        
+        
+        return answer;
+    }
+}
