6a44e0f38465159d9ae61a332ac100076c769bfe
yu <yurimkang20@gmail.com>
2024-06-25 13:29:23 +0900

    upload. [PCCP 기출문제] 붕대 감기

---
+++ python/PCCP 기출문제/붕대 감기.py
@@ -0,0 +1,11 @@
+
+""" [PCCP 기출문제] 1번. 붕대 감기 """
+
+def solution(bandage, health, attacks):
+    answer = 0
+    return answer
+
+print(solution([5, 1, 5], 30, [[2, 10], [9, 15], [10, 5], [11, 5]]))    # 5
+print(solution([3, 2, 7], 20, [[1, 15], [5, 16], [8, 6]]))              # -1
+print(solution([4, 2, 7], 20, [[1, 15], [5, 16], [8, 6]]))              # -1
+print(solution([1, 1, 1], 20, [[1, 2], [3, 2]]))                        # 3
