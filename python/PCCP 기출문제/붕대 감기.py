7538216882ff66c66a25110042d69fc90a6c4b89
yu <yurimkang20@gmail.com>
2024-06-25 14:14:03 +0900

    upload. [PCCP 기출문제] 붕대 감기

--- python/PCCP 기출문제/붕대 감기.py
+++ python/PCCP 기출문제/붕대 감기.py
@@ -2,8 +2,28 @@
 """ [PCCP 기출문제] 1번. 붕대 감기 """
 
 def solution(bandage, health, attacks):
-    answer = 0
-    return answer
+    
+    now_time = 0
+    now_health = health
+    
+    for at, ad in attacks:
+        
+        temp_time = at - now_time - 1
+        
+        now_health += (temp_time * bandage[1])
+        now_health += (temp_time // bandage[0]) * bandage[2] 
+        
+        if now_health >= health:
+            now_health = health
+            
+        now_health -= ad
+        
+        if now_health <= 0:
+            return -1
+        
+        now_time = at
+
+    return now_health
 
 print(solution([5, 1, 5], 30, [[2, 10], [9, 15], [10, 5], [11, 5]]))    # 5
 print(solution([3, 2, 7], 20, [[1, 15], [5, 16], [8, 6]]))              # -1
