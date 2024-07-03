52d45d194a6659ba05c2a7443a5cf2cb11dc3cf9
yu <yurimkang20@gmail.com>
2024-07-03 22:41:41 +0900

    upload. [PCCP 모의고사 1회] 체육대회

--- python/PCCP 모의고사 1회/체육대회.py
+++ python/PCCP 모의고사 1회/체육대회.py
@@ -1,2 +1,23 @@
 
 """ [PCCP 모의고사 1회] 2번. 체육대회 """
+
+from itertools import permutations
+
+def solution(ability):
+    
+    sport_count = len(ability[0])
+    student = [num for num in range(len(ability))]
+    
+    answer = 0
+    for st in list(permutations(student, sport_count)):
+        
+        count = 0
+        for sport in range(sport_count):
+            count += ability[st[sport]][sport]
+        
+        answer = max(answer, count)
+    
+    return answer
+
+print(solution([[40, 10, 10], [20, 5, 0], [30, 30, 30], [70, 0, 70], [100, 100, 100]]))         # 210
+print(solution([[20, 30], [30, 20], [20, 30]]))                                                 # 60
