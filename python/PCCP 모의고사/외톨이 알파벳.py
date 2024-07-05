42c81608b69fa4717d7e1baaad5ce171d1a166e8
yu <yurimkang20@gmail.com>
2024-07-05 14:41:12 +0900

    upload. [PCCP 모의고사] 외톨이 알파벳

---
+++ python/PCCP 모의고사/외톨이 알파벳.py
@@ -0,0 +1,27 @@
+
+""" [PCCP 모의고사] 1번. 외톨이 알파벳 """
+
+def solution(input_string):
+
+    answer = set()
+    word = set()
+
+    length = len(input_string)
+    i = 0
+    while i < length:
+        
+        temp = input_string[i]
+        while i < length and input_string[i] == temp:
+            i += 1
+
+        if temp in word:
+            answer.add(temp)
+        else:
+            word.add(temp)
+    
+    return "".join(sorted(list(answer))) if answer else "N"
+
+print(solution("edeaaabbccd"))      # "de"
+print(solution("eeddee"))           # "e"
+print(solution("string"))           # "N"
+print(solution("zbzbz"))            # bz
